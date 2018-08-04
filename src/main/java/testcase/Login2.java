06fa6afedd85c2de960953f1a122c2270c941eaf
xanedumaran <xanedumaran@LDO-PKT-NBT032.ldo.net>
2018-08-04 18:07:32 +0530

    New test case added

---
+++ src/main/java/testcase/Login2.java
@@ -0,0 +1,28 @@
+package testcase;
+
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+
+import wd.SeMethods;
+
+public class Login2 extends SeMethods{
+
+	@Test
+	public void login() {
+		startApp("chrome", "http://leaftaps.com/opentaps");
+		WebElement eleUserName = locateElement("id", "username");
+		type(eleUserName, "DemoSalesManager");
+		WebElement elePassword = locateElement("id","password");
+		type(elePassword, "crmsfa");
+		WebElement eleLogin = locateElement("class","decorativeSubmit");
+		click(eleLogin);
+		
+	}
+	
+}
+
+
+
+
+
+
